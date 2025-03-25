d8b72078f33e6b5b2fe8d8259155c7b8aa28185f
Asoel <asoel@iscte-iul.pt>
2025-03-25 16:32:28 +0000

    Update: *.html files & script.js
    -Adicionar bootstrap aos ficheiros
    -Corrigir o erro do botão menu e centralizá-lo
    -Corrigir o footer
    -Melhorar o script do toggleMenu
    -criar um ficheiro externo formulario.css

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,9 +1,19 @@
 function toggleAccountMenu() {
     const menu = document.getElementById('accountMenu');
     if (menu) {
+        // Abre ou fecha o menu ao clicar no botão
         menu.classList.toggle('show');
     }
 }
 
+// Detectar quando o mouse sai da área do menu ou do botão
+const accountBtn = document.querySelector('.account-btn');
+const accountMenu = document.getElementById('accountMenu');
 
-
+// Fechar o menu quando o mouse sair de qualquer área
+document.addEventListener('mousemove', (event) => {
+    const isMouseOutsideMenu = !accountMenu.contains(event.target) && !accountBtn.contains(event.target);
+    if (isMouseOutsideMenu && accountMenu.classList.contains('show')) {
+        accountMenu.classList.remove('show');
+    }
+});
