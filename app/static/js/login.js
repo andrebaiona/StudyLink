188149c93be2abada605caa8edc55e08aeaf3818
Asoel <asoel@iscte-iul.pt>
2025-05-25 23:55:00 +0100

    fix login & registo
    Troquei os antigos .css e .js pelos novos, para ficar mais organizado e limpo

--- app/static/js/login.js
+++ app/static/js/login.js
@@ -60,3 +60,15 @@ function togglePassword(fieldId, iconElement) {
     eyeIcon.classList.add('fa-eye');
   }
 }
+
+
+// Função para alternar o tema
+function toggleTheme() {
+    const body = document.body;
+    const toggleIcon = document.querySelector('.theme-icon');
+    body.classList.toggle('dark-mode');
+    toggleIcon.innerHTML = body.classList.contains('dark-mode')
+        ? '<path d="M12 3a9 9 0 009 9 9 9 0 00-9 9 9 9 0 00-9-9 9 9 0 009-9z" />'
+        : '<path d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z" />';
+    localStorage.setItem('theme', body.classList.contains('dark-mode') ? 'dark' : 'light');
+}
--- app/static/js/registo.js
+++ app/static/js/registo.js
@@ -1,3 +1,22 @@
+// Função para alternar o tema
+function toggleTheme() {
+    const body = document.body;
+    const toggleIcon = document.querySelector('.theme-icon');
+    body.classList.toggle('dark-mode');
+    toggleIcon.innerHTML = body.classList.contains('dark-mode')
+        ? '<path d="M12 3a9 9 0 009 9 9 9 0 00-9 9 9 9 0 00-9-9 9 9 0 009-9z" />'
+        : '<path d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z" />';
+    localStorage.setItem('theme', body.classList.contains('dark-mode') ? 'dark' : 'light');
+}
+
+// Verificar tema guardado ao carregar a página
+document.addEventListener('DOMContentLoaded', () => {
+    if (localStorage.getItem('theme') === 'dark') {
+        document.body.classList.add('dark-mode');
+        document.querySelector('.theme-icon').innerHTML = '<path d="M12 3a9 9 0 009 9 9 9 0 00-9 9 9 9 0 00-9-9 9 9 0 009-9z" />';
+    }
+});
+
 /* Javascript para Medidor de força da Password */
 document.addEventListener("DOMContentLoaded", function () {
     const passwordInput = document.getElementById("password");
